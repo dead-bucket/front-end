a7ff996486922d05066991d4034e1897e1d19582
Roger <roger.neil.davenport@gmail.com>
2019-02-24 13:25:59 -0800

    changed store setup

--- src/components/friendView/FriendView4.js
+++ src/components/friendView/FriendView4.js
@@ -106,7 +106,7 @@ class FriendView4 extends Component {
     const { target } = this.props.profile;
     const { value } = this.state;
 
-    console.log(isUser);
+    
     let actionContent;
     switch (value) {
       case 0:
