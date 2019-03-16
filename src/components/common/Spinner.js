94f3a49e60c2064b7598096b4d3209d095be2e7e
Denis Molloy <denis.molloy@gmail.com>
2019-03-16 16:54:55 -0700

    updated icon

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,13 +1,18 @@
 import React from "react";
-import spinner from "./spinner2.gif";
+import spinner from "./spinner.gif";
+const spinnerStyle = {
+  height: "500px",
+  width: "100vw",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+};
 export default function Spinner() {
   return (
-    <div>
-      <img
-        src={spinner}
-        alt="Loading..."
-        style={{ width: "200px", margin: "auto", display: "block" }}
-      />
-    </div>
+    <img
+      src={spinner}
+      alt="Loading..."
+      style={{ width: "200px", margin: "auto", display: "block" }}
+    />
   );
 }
