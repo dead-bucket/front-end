04f9b6eb5ee711811e56557151bacdc5ed1f9c50
Denis Molloy <denis.molloy@gmail.com>
2019-03-16 23:58:43 -0700

    cleaned up spinner/fav

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,12 +1,6 @@
 import React from "react";
 import spinner from "./spinner.gif";
-const spinnerStyle = {
-  height: "500px",
-  width: "100vw",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center"
-};
+
 export default function Spinner() {
   return (
     <img
