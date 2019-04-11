222135480792c6f9416af8a9793272040e02b039
Roger <roger.neil.davenport@gmail.com>
2019-04-11 11:52:21 -0700

    changed api to export based on NODE-ENV

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,3 +1,9 @@
 // const API = "https://dead-bucket-back-end.herokuapp.com";
-const API = "http://localhost:4000";
+// const API = "http://localhost:4000";
+let API;
+if (process.env.NODE_ENV === 'production') {
+  API = "https://dead-bucket-back-end.herokuapp.com";
+} else {
+  API = "http://localhost:4000";
+}
 export default API;
