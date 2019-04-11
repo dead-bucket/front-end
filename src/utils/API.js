441dadc15b2e44bcf7e4d7f9c7a7d43c16323b49
Roger <roger.neil.davenport@gmail.com>
2019-04-11 12:00:55 -0700

    API changes

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,4 @@
-// const API = "https://dead-bucket-back-end.herokuapp.com";
+const API = "https://dead-bucket-back-end.herokuapp.com";
 // const API = "http://localhost:4000";
-let API;
-if (process.env.NODE_ENV === 'production') {
-  API = "https://dead-bucket-back-end.herokuapp.com";
-} else {
-  API = "http://localhost:4000";
-}
+
 export default API;
