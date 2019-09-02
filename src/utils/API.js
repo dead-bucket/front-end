e94390084d0d5abefc986540555d7ef3c20c9da2
Roger <roger.neil.davenport@gmail.com>
2019-09-01 19:33:47 -0700

    changed API file

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,4 +1,10 @@
-// const API = "https://dead-bucket-back-end.herokuapp.com";
-const API = "http://localhost:4000";
+let API;
+if (process.env.REACT_APP_ENV === "production") {
+  API = "https://dead-bucket-back-end.herokuapp.com";
+} else {
+  API = "http://localhost:4000";
+
+}
+
 
 export default API;
